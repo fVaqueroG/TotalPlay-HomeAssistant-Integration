58e6beac6e5cb333415e9b28e31312088a13ecc8
fVaqueroG <33148541+fVaqueroG@users.noreply.github.com>
2026-09-21 11:47:49 -0600

    Cover XMLTV coverage diagnostics and timezone-aware minute timestamps

--- tests/test_epg_data.py
+++ tests/test_epg_data.py
@@ -20,15 +20,35 @@ class XmltvTests(unittest.TestCase):
         </tv>'''
         result = module.parse_xmltv(xml, datetime(2026, 9, 21, 14, 30, tzinfo=timezone.utc))
         self.assertEqual(result['programme_count'], 3)
+        self.assertEqual(result['valid_timestamp_count'], 3)
+        self.assertEqual(result['window_programme_count'], 2)
+        self.assertEqual(result['scheduled_channel_count'], 1)
         self.assertEqual(result['channels'][0]['id'], 'A&E.mx')
         self.assertEqual([p['title'] for p in result['channels'][0]['schedule']], ['Now & Then', 'Next'])
         self.assertEqual(result['channels'][0]['schedule'][0]['start'], '2026-09-21T14:00:00+00:00')
+
+    def test_explicit_timezone_without_seconds_is_supported(self):
+        xml = b'''<tv><channel id="id"><display-name>Channel</display-name></channel>
+        <programme channel="id" start="202609210830 -0600" stop="202609210930 -0600"><title>Now</title></programme></tv>'''
+        result = module.parse_xmltv(xml, datetime(2026, 9, 21, 15, 0, tzinfo=timezone.utc))
+        self.assertEqual(result['channels'][0]['schedule'][0]['title'], 'Now')
+        self.assertEqual(result['window_programme_count'], 1)
+
+    def test_stale_guide_is_not_misreported_as_current_programming(self):
+        xml = b'''<tv><channel id="id"><display-name>Channel</display-name></channel>
+        <programme channel="id" start="20260801083000 -0600" stop="20260801093000 -0600"><title>Old</title></programme></tv>'''
+        result = module.parse_xmltv(xml, datetime(2026, 9, 21, 15, 0, tzinfo=timezone.utc))
+        self.assertEqual(result['programme_count'], 1)
+        self.assertEqual(result['valid_timestamp_count'], 1)
+        self.assertEqual(result['window_programme_count'], 0)
+        self.assertEqual(result['scheduled_channel_count'], 0)
 
     def test_missing_timezone_is_not_guessed(self):
         xml = b'''<tv><channel id="id"><display-name>Channel</display-name></channel>
         <programme channel="id" start="20260921140000" stop="20260921150000"><title>Unknown zone</title></programme></tv>'''
         result = module.parse_xmltv(xml, datetime(2026, 9, 21, 14, 30, tzinfo=timezone.utc))
         self.assertEqual(result['channels'][0]['schedule'], [])
+        self.assertEqual(result['valid_timestamp_count'], 0)
 
     def test_rejects_dtd_and_invalid_document(self):
         with self.assertRaisesRegex(ValueError, 'DTD'):
