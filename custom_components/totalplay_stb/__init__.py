512ed3d9a54370ab283ad7aa8b06de738a098e3a
fVaqueroG <33148541+fVaqueroG@users.noreply.github.com>
2026-09-22 16:17:50 -0600

    Load Totalplay card v0.3.56

--- custom_components/totalplay_stb/__init__.py
+++ custom_components/totalplay_stb/__init__.py
@@ -28,7 +28,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         await hass.http.async_register_static_paths(
             [
                 StaticPathConfig(
-                    _CARD_URL, str(www / "totalplay-stb-v0355.js"), False
+                    _CARD_URL, str(www / "totalplay-stb-v0356.js"), False
                 ),
                 *(
                     StaticPathConfig(
